c7849bac1a5e78420d89082a2f5b25e3d01817fa
xElon-Mask <xelonmask@protonmail.com>
2023-07-03 13:16:38 +0200

    built app files tree + header

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header";
+import "../../App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
