4fb5deac43ca7c39f7a8d971aa64894e3825c749
xElon-Mask <xelonmask@protonmail.com>
2023-07-04 18:08:57 +0200

    create Header, Landing, Footer components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../Header";
+import Landing from "../Landing";
+import Footer from "../Footer";
 import "../../App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Landing />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>
+          Projet réalisé par xElon Mask pour la formation ReactJS de DonkeyGeek
+          - 2023
+        </p>
+        <p>
+          Les icônes Wolverine, Iron-Man, Spider-man, Batman sont prises sur le
+          site iconFinder.com
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
